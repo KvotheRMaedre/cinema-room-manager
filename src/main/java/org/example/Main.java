64a2eebf53c34aa7a1f5129ba58abcef5327c2c8
Ítalo H. <italodssouza.backup@gmail.com>
2025-05-20 20:08:06 -0300

    Calculating profit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     static int row = 7;
     static int column = 8;
@@ -8,6 +10,7 @@ public class Main {
     public static void main(String[] args) {
         initializeRoom();
         printSeats();
+        createNewRoom();
     }
 
     public static void initializeRoom(){
@@ -29,5 +32,38 @@ public class Main {
             }
             System.out.println(linesS);
         }
+    }
+
+    public static void createNewRoom(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Now lets add another room to the cinema.");
+        System.out.println("Enter the number of rows:");
+        int numRow = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int numColumn = scanner.nextInt();
+
+        int profit = calculateProfit(numRow, numColumn);
+        System.out.println("Total income:\n$" + profit);
+    }
+
+    public static int calculateProfit(int numRows, int numColumns){
+        int profit;
+        int priceFirstRow = 10;
+        int priceLastRow = 8;
+        int totalSeats = numRows * numColumns;
+        if (totalSeats <= 60){
+            profit = totalSeats * priceFirstRow;
+        } else {
+            profit = ((numRows / 2) * numColumns) * priceFirstRow;
+
+            if (numRows % 2 != 0){
+                profit += ((numRows - (numRows / 2)) * numColumns) * priceLastRow;
+            } else {
+                profit += ((numRows / 2) * numColumns) * priceLastRow;
+            }
+        }
+
+        return profit;
     }
 }
